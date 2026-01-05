3777283148c3c3172f14d02399b8c0453572f0ab
Pedro Christovam <pedrochristovam053@gmail.com>
2026-01-05 11:30:28 -0300

    Fix Sass alias resolution

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const srcPath = path.resolve(process.cwd(), 'src')
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': srcPath,
     },
   },
 })
